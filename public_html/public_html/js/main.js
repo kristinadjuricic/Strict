de7c4a01492e0f107ab595a44dc9f1fcdc246dd0
Kristina Djuricic <kristinadjuricic11@gmail.com>
2020-03-19 21:26:40 +0100

    Initialize a project

---
+++ public_html/public_html/js/main.js
@@ -0,0 +1,69 @@
+$(document).ready(function () {
+
+    if ($('.contact-form').length > 0) {
+
+        $(function () {
+            $(".contact-form").validate({
+                highlight: function (element) {
+                    $(element).addClass("is-invalid").removeClass("is-valid");
+                },
+                unhighlight: function (element) {
+                    $(element).removeClass('is-invalid').addClass('is-valid');
+                },
+                rules: {
+                    name: {
+                        required: true
+                    },
+                    email: {
+                        required: true,
+                        email: true
+                    },
+                    message: {
+                        required: true
+                    }
+
+                },
+                messages: {
+
+                    name: {
+                        required: '*The Name is required field.*'
+                    },
+                    email: {
+                        required: '*The Email is required field.*',
+                        email: '*Please provide a valid email address.*'
+                    },
+                    message: {
+                        required: '*The Message is required field.*'
+                    }
+
+                },
+                errorElement: 'p',
+                errorPlacement: function (error, element) {
+                    error.appendTo($(element).closest('.form-group').find('.invalid-feedback'));
+                }
+
+            });
+        });
+    }
+
+    function animation() {
+        var windowHeight = $(window).height();
+        var scroll = $(window).scrollTop();
+        $('.animation').each(function () {
+            let position = $(this).offset().top;
+            let animation = $(this).attr('data-animation');
+            let delay = $(this).attr('data-delay');
+            if (position < scroll + windowHeight - 100) {
+                $(this).css('animation-delay', delay);
+                $(this).addClass(animation);
+            }
+        });
+    }
+    animation();
+    $(window).scroll(function () {
+        animation();
+    });
+
+
+});
+
